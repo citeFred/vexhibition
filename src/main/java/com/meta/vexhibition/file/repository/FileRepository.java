1b1eef91e336005e4d672f720ab843d191c92f35
citeFred <citefred@gmail.com>
2025-09-13 17:54:40 +0900

    refactor: paths 배열로 변경한 수정페이지 추가

--- src/main/java/com/meta/vexhibition/file/repository/FileRepository.java
+++ src/main/java/com/meta/vexhibition/file/repository/FileRepository.java
@@ -3,5 +3,8 @@ package com.meta.vexhibition.file.repository;
 import com.meta.vexhibition.file.domain.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FileRepository extends JpaRepository<File, Long> {
+    List<File> findByOriginalFileNameStartsWith(String baseName);
 }
